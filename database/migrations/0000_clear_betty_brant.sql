47bf961540255ffe36d1949deeb4ccecc90f7123
Chemist <arnoldmusandu@gmail.com>
2024-12-30 16:25:01 +0300

    create the sql file for migrations adding the vector extension for embeddings vector for bible chat

--- database/migrations/0000_clear_betty_brant.sql
+++ database/migrations/0000_clear_betty_brant.sql
@@ -1,6 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS vector;
+
 CREATE TABLE "analytics" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"user_id" integer NOT NULL,
+	"user_id" uuid NOT NULL,
 	"metrics" text,
 	"value" jsonb,
 	"createdAt" timestamp DEFAULT now()
@@ -16,7 +18,7 @@ CREATE TABLE "bible" (
 --> statement-breakpoint
 CREATE TABLE "chats" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"user_id" integer NOT NULL,
+	"user_id" uuid NOT NULL,
 	"message" text,
 	"response" text,
 	"createdAt" timestamp DEFAULT now()
@@ -24,13 +26,23 @@ CREATE TABLE "chats" (
 --> statement-breakpoint
 CREATE TABLE "cronJobs" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"user_id" integer NOT NULL,
+	"user_id" uuid NOT NULL,
 	"status" text,
 	"job_id" text
 );
 --> statement-breakpoint
+CREATE TABLE "embeddings" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"user_id" uuid NOT NULL,
+	"content" text,
+	"embedding" vector(768),
+	"metadata" jsonb,
+	"createdAt" timestamp DEFAULT now()
+);
+--> statement-breakpoint
 CREATE TABLE "users" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text NOT NULL,
 	"email" text NOT NULL,
 	"createdAt" timestamp DEFAULT now(),
 	CONSTRAINT "users_email_unique" UNIQUE("email")
@@ -38,4 +50,5 @@ CREATE TABLE "users" (
 --> statement-breakpoint
 ALTER TABLE "analytics" ADD CONSTRAINT "analytics_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "chats" ADD CONSTRAINT "chats_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "cronJobs" ADD CONSTRAINT "cronJobs_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
+ALTER TABLE "cronJobs" ADD CONSTRAINT "cronJobs_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "embeddings" ADD CONSTRAINT "embeddings_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
